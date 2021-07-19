801cc10eb51a1a9dcd0bd3c470e9ab1b8b8a4a61
AlbaOL <aortegalorenzo@gmail.com>
2021-07-19 16:15:47 +0200

    adding some testing users via seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-9.times do |i|
-    Recipe.create(
-      name: "Recipe #{i + 1}",
-      ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
-      instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
-    )
-  end
+# 9.times do |i|
+#     Recipe.create(
+#       name: "Recipe #{i + 1}",
+#       ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+#       instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+#     )
+# end
+
+2.times do |i|
+  User.create!(
+    email: "rider#{i}@gmail.com",
+    password: "Rider1234!",
+    password_confirmation: "Rider1234!",
+    role: 0
+  ) unless User.find_by(email: "rider#{i}@gmail.com").present?
+end
+2.times do |i|
+  User.create!(
+    email: "ba#{i}@gmail.com",
+    password: "Ba1234!",
+    password_confirmation: "Ba1234!",
+    role: 1
+  ) unless User.find_by(email: "ba#{i}@gmail.com").present?
+end
